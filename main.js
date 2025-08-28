8847b9d024737c7aa4232395037f8082dafe0c59
Jenna <abiscc14@gmail.com>
2025-08-28 11:26:19 +0800

    新增首頁區塊1、2

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 import './assets/scss/all.scss';
 
 console.log('Hello world');
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: "auto",
+  loop: true,
+  initialSlide: 3,
+  centeredSlides: true,
+  spaceBetween: 12,
+  breakpoints: {
+    992: {
+      spaceBetween: 40,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
