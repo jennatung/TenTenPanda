d81757ed3d1f0a97784c5ceb660eb58c9378bcb2
Hsiaowen320 <python.hkw@gmail.com>
2025-08-30 03:54:32 +0800

    首頁初稿確認

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
+import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log('Hello world');
+// 下拉選單的 icon 變化效果
+const dropdownEl = document.getElementById("productList");
+const iconEl = document.getElementById("dropdownIcon");
+
+dropdownEl.addEventListener("show.bs.dropdown", () => {
+  iconEl.textContent = "keyboard_arrow_up";
+});
+
+dropdownEl.addEventListener("hide.bs.dropdown", () => {
+  iconEl.textContent = "keyboard_arrow_down";
+});
+
+// Swiper 動畫效果
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: "auto",
+  loop: true,
+  initialSlide: 3,
+  centeredSlides: true,
+  spaceBetween: 12,
+  breakpoints: {
+    992: {
+      spaceBetween: 40,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
